908c512f2721792ef4852e0c257492f3c9baf837
michalkonecny <mikkonecny@gmail.com>
2024-05-03 22:52:01 +0100

    move sets and pavings to a new module

--- src/BranchAndPrune/BranchAndPrune.hs
+++ src/BranchAndPrune/BranchAndPrune.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FunctionalDependencies #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE RecordWildCards #-}
@@ -11,17 +10,8 @@
 {-# LANGUAGE UndecidableInstances #-}
 
 module BranchAndPrune.BranchAndPrune
-  ( IsSet (..),
-    SetFromBasic (..),
-    CanPrune (..),
-    CanSplitSet (..),
-    Paving (..),
-    pavingInner,
-    pavingOuter,
-    pavingUndecided,
-    pavingInnerOuter,
-    pavingInnerUndecided,
-    pavingOuterUndecided,
+  ( 
+    module BranchAndPrune.Sets,
     IsPriorityQueue (..),
     Params (..),
     Result (..),
@@ -36,27 +26,13 @@ import qualified Data.Text as T
 import BranchAndPrune.ForkUtils (HasIsAborted (..), forkAndMerge)
 import GHC.Conc (numCapabilities)
 import Text.Printf (printf)
+import BranchAndPrune.Sets
 
 -- The following wrapper supports the use of "printf" to format log messages.
 -- It also reduced the need for OverloadedStrings.
 -- OverloadedStrings is not sufficient to get `printf` to work with the logger functions.
 logDebugStr :: (MonadLogger m) => String -> m ()
 logDebugStr = logDebugN . T.pack
-
-class IsSet set where
-  emptySet :: set
-  setIsEmpty :: set -> Bool
-  setShowStats :: set -> String
-  setUnion :: set -> set -> set
-
-class SetFromBasic basicSet set where
-  fromBasicSets :: [basicSet] -> set
-
-class CanSplitSet basicSet set where
-  splitSet :: set -> [basicSet] -- at least two so that B&P makes progress
-
-class CanPrune m basicSet constraint set where
-  pruneBasicSetM :: constraint -> basicSet -> m (constraint, Paving set)
 
 class IsPriorityQueue priorityQueue elem | priorityQueue -> elem where
   singletonQueue :: elem -> priorityQueue
@@ -65,58 +41,6 @@ class IsPriorityQueue priorityQueue elem | priorityQueue -> elem where
   queueAddMany :: priorityQueue -> [elem] -> priorityQueue
   queueSplit :: priorityQueue -> Maybe (priorityQueue, priorityQueue)
   queueMerge :: priorityQueue -> priorityQueue -> priorityQueue
-
-data Paving set = Paving
-  { inner :: set,
-    undecided :: set,
-    outer :: set
-  }
-  deriving (Eq, Show)
-
-showPavingSummary :: (IsSet set) => Paving set -> String
-showPavingSummary (Paving {..}) =
-  printf "{inner: %s, undecided: %s, outer: %s}" (setShowStats inner) (setShowStats undecided) (setShowStats outer)
-
-emptyPaving :: (IsSet set) => Paving set
-emptyPaving =
-  Paving
-    { inner = emptySet,
-      undecided = emptySet,
-      outer = emptySet
-    }
-
-pavingMerge :: (IsSet set) => Paving set -> Paving set -> Paving set
-pavingMerge paving1 paving2 =
-  paving1
-    { inner = paving1.inner `setUnion` paving2.inner,
-      undecided = paving1.undecided `setUnion` paving2.undecided,
-      outer = paving1.outer `setUnion` paving2.outer
-    }
-
-pavingAddDecided :: (IsSet set) => Paving set -> Paving set -> Paving set
-pavingAddDecided paving1 paving2 =
-  paving1
-    { inner = paving1.inner `setUnion` paving2.inner,
-      outer = paving1.outer `setUnion` paving2.outer
-    }
-
-pavingInner :: (IsSet set) => set -> Paving set
-pavingInner inner = Paving {inner, undecided = emptySet, outer = emptySet}
-
-pavingOuter :: (IsSet set) => set -> Paving set
-pavingOuter outer = Paving {inner = emptySet, undecided = emptySet, outer}
-
-pavingUndecided :: (IsSet set) => set -> Paving set
-pavingUndecided undecided = Paving {undecided, inner = emptySet, outer = emptySet}
-
-pavingInnerOuter :: (IsSet set) => set -> set -> Paving set
-pavingInnerOuter inner outer = Paving {inner, undecided = emptySet, outer}
-
-pavingInnerUndecided :: (IsSet set) => set -> set -> Paving set
-pavingInnerUndecided inner undecided = Paving {inner, undecided, outer = emptySet}
-
-pavingOuterUndecided :: (IsSet set) => set -> set -> Paving set
-pavingOuterUndecided outer undecided = Paving {inner = emptySet, undecided, outer}
 
 data Params m basicSet set priorityQueue constraint = ParamsM
   { scope :: basicSet,
---
+++ src/BranchAndPrune/Sets.hs
@@ -0,0 +1,94 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DisambiguateRecordFields #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+module BranchAndPrune.Sets
+  ( IsSet (..),
+    SetFromBasic (..),
+    CanSplitSet (..),
+    CanPrune (..),
+    Paving (..),
+    showPavingSummary,
+    emptyPaving,
+    pavingMerge,
+    pavingAddDecided,
+    pavingInner,
+    pavingOuter,
+    pavingUndecided,
+    pavingInnerOuter,
+    pavingInnerUndecided,
+    pavingOuterUndecided,
+  )
+where
+import Text.Printf (printf)
+
+class IsSet set where
+  emptySet :: set
+  setIsEmpty :: set -> Bool
+  setShowStats :: set -> String
+  setUnion :: set -> set -> set
+
+class SetFromBasic basicSet set where
+  fromBasicSets :: [basicSet] -> set
+
+class CanSplitSet basicSet set where
+  splitSet :: set -> [basicSet] -- at least two so that B&P makes progress
+
+class CanPrune m basicSet constraint set where
+  pruneBasicSetM :: constraint -> basicSet -> m (constraint, Paving set)
+
+data Paving set = Paving
+  { inner :: set,
+    undecided :: set,
+    outer :: set
+  }
+  deriving (Eq, Show)
+
+showPavingSummary :: (IsSet set) => Paving set -> String
+showPavingSummary (Paving {..}) =
+  printf "{inner: %s, undecided: %s, outer: %s}" (setShowStats inner) (setShowStats undecided) (setShowStats outer)
+
+emptyPaving :: (IsSet set) => Paving set
+emptyPaving =
+  Paving
+    { inner = emptySet,
+      undecided = emptySet,
+      outer = emptySet
+    }
+
+pavingMerge :: (IsSet set) => Paving set -> Paving set -> Paving set
+pavingMerge paving1 paving2 =
+  paving1
+    { inner = paving1.inner `setUnion` paving2.inner,
+      undecided = paving1.undecided `setUnion` paving2.undecided,
+      outer = paving1.outer `setUnion` paving2.outer
+    }
+
+pavingAddDecided :: (IsSet set) => Paving set -> Paving set -> Paving set
+pavingAddDecided paving1 paving2 =
+  paving1
+    { inner = paving1.inner `setUnion` paving2.inner,
+      outer = paving1.outer `setUnion` paving2.outer
+    }
+
+pavingInner :: (IsSet set) => set -> Paving set
+pavingInner inner = Paving {inner, undecided = emptySet, outer = emptySet}
+
+pavingOuter :: (IsSet set) => set -> Paving set
+pavingOuter outer = Paving {inner = emptySet, undecided = emptySet, outer}
+
+pavingUndecided :: (IsSet set) => set -> Paving set
+pavingUndecided undecided = Paving {undecided, inner = emptySet, outer = emptySet}
+
+pavingInnerOuter :: (IsSet set) => set -> set -> Paving set
+pavingInnerOuter inner outer = Paving {inner, undecided = emptySet, outer}
+
+pavingInnerUndecided :: (IsSet set) => set -> set -> Paving set
+pavingInnerUndecided inner undecided = Paving {inner, undecided, outer = emptySet}
+
+pavingOuterUndecided :: (IsSet set) => set -> set -> Paving set
+pavingOuterUndecided outer undecided = Paving {inner = emptySet, undecided, outer}
+
