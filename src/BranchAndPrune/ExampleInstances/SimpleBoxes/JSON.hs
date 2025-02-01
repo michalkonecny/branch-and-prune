0b269327641848ec080fd9b8668b232e926f7c23
Michal Konecny <mikkonecny@gmail.com>
2025-02-01 21:57:18 +0000

    export Form to JSON with its structure

--- src/BranchAndPrune/ExampleInstances/SimpleBoxes/JSON.hs
+++ src/BranchAndPrune/ExampleInstances/SimpleBoxes/JSON.hs
@@ -3,9 +3,9 @@
 module BranchAndPrune.ExampleInstances.SimpleBoxes.JSON () where
 
 import AERN2.MP qualified as MP
-
-import BranchAndPrune.ExampleInstances.RealConstraints (Expr (..), Form (..))
+import BranchAndPrune.ExampleInstances.RealConstraints (Expr (..), Form (..), BinaryComp, BinaryConn, UnaryConn)
 import BranchAndPrune.ExampleInstances.SimpleBoxes.Boxes (Box (..), Boxes (..))
+import Data.Aeson (ToJSON (toJSON), (.=))
 import Data.Aeson qualified as A
 import GHC.Records ()
 import MixedTypesNumPrelude
@@ -26,11 +26,19 @@ instance A.ToJSON MP.MPBall where
       (l, u) = MP.endpoints b
       lD = double l
       uD = double u
-      (.=) = (A..=)
 
 instance A.ToJSON (Expr b r) where
   toJSON e = A.toJSON e.description -- a dummy String JSON instance
 
-instance A.ToJSON (Form (Expr b r)) where
-  toJSON = A.toJSON . show -- a dummy String JSON instance
+instance A.ToJSON BinaryComp where
+  toJSON bc = A.toJSON (show bc)
+
+instance A.ToJSON UnaryConn where
+  toJSON uc = A.toJSON (show uc)
+
+instance A.ToJSON BinaryConn where
+  toJSON bc = A.toJSON (show bc)
+
+instance (A.ToJSON e) => A.ToJSON (Form e) where
+  toEncoding = A.genericToEncoding A.defaultOptions
 
