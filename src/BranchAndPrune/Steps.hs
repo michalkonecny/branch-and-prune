c496038d44c328ae79fd7acd0b015bd7f9428294
Michal Konecny <mikkonecny@gmail.com>
2026-01-17 23:40:29 +0000

    evalInfo produced by pruning -> progress step record

--- src/BranchAndPrune/Steps.hs
+++ src/BranchAndPrune/Steps.hs
@@ -2,19 +2,22 @@
 
 module BranchAndPrune.Steps
   ( Step (..),
-  getStepProblems,
-  getStepPavings)
+    getStepProblems,
+    getStepPavings,
+  )
 where
 
+import BranchAndPrune.Paving (Paving (..), Problem)
 import GHC.Generics
 
-data Step problem paving
+data Step problem paving evalInfo
   = InitStep
       { problem :: problem
       }
   | ProgressStep
       { problem :: problem,
-        progressPaving :: paving
+        progressPaving :: paving,
+        evalInfo :: evalInfo
       }
   | GiveUpOnProblemStep
       { problem :: problem
@@ -25,15 +28,20 @@ data Step problem paving
   | DoneStep
   deriving (Show, Generic)
 
-getStepProblems :: Step problem paving -> [problem]
+getStepProblems ::
+  ( problem ~ Problem constraint basicSet,
+    paving ~ Paving constraint basicSet set
+  ) =>
+  Step problem paving evalInfo ->
+  [problem]
 getStepProblems step = case step of
   InitStep {problem} -> [problem]
-  ProgressStep {problem} -> [problem]
+  ProgressStep {problem, progressPaving} -> problem : progressPaving.undecided
   GiveUpOnProblemStep {problem} -> [problem]
   AbortStep {} -> []
   DoneStep {} -> []
 
-getStepPavings :: Step problem paving -> [paving]
+getStepPavings :: Step problem paving evalInfo -> [paving]
 getStepPavings step = case step of
   ProgressStep {progressPaving} -> [progressPaving]
   _ -> []
